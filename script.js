a706f24339a7e5047dcdbb4281fee034b9cdaaa1
Mukul <mukulkashyap.business@gmail.com>
2026-01-09 16:09:31 +0530

    fix the argument of appendChild method

--- script.js
+++ script.js
@@ -6,5 +6,5 @@ const TOTAL_SQUARES = GRID_SIDE * GRID_SIDE;
 for(let i = 0; i < TOTAL_SQUARES; i++){
     const square = document.createElement("div");
     square.classList.add("square");
-    container.appendChild("square");
+    container.appendChild(square);
 }

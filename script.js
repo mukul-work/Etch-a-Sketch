7c025e0656dc9648b2407a92ce210686ab20921d
Mukul <mukulkashyap.business@gmail.com>
2026-01-09 17:17:42 +0530

    replace hover with click-and-drag

--- script.js
+++ script.js
@@ -1,13 +1,45 @@
 const container = document.querySelector("#container");
+const btn = document.querySelector("#resize-btn")
 
-const GRID_SIDE = 16;
-const TOTAL_SQUARES = GRID_SIDE * GRID_SIDE;
+let isDrawing = false;
 
-for(let i = 0; i < TOTAL_SQUARES; i++){
-    const square = document.createElement("div");
-    square.classList.add("square");
-    square.addEventListener("mouseenter", () => {
-        square.classList.add("hover");
-    })
-    container.appendChild(square);
+document.body.addEventListener("mousedown", () => {
+    isDrawing = true;
+});
+
+document.body.addEventListener("mouseup", () => {
+    isDrawing = false;
+})
+
+
+
+function createGrid(size){
+    container.innerHTML = "";
+
+    container.style.gridTemplateColumns = `repeat(${size}, 1fr)`;
+
+    for(let i = 0; i < size * size; i++){
+        const square = document.createElement("div");
+        square.classList.add("square");
+
+        square.addEventListener("mouseenter", () => {
+            if(isDrawing){
+                square.classList.add("hover");
+            }
+        });
+
+        container.appendChild(square);
+    }
 }
+
+createGrid(16);
+
+btn.addEventListener("click", () => {
+    let numberOfSquares = Number(prompt("Enter the number of squares per side(max 100): "));
+    if(!numberOfSquares || numberOfSquares < 1 || numberOfSquares > 100){
+        alert("Please enter a number between 1 and 100");
+        return;
+    }
+
+    createGrid(numberOfSquares);
+});
